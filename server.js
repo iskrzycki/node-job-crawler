e158d4c6c381916f88f0bc1f5fafb5a86bf689d3
Rafał Iskrzycki <iskrzycki@gmail.com>
2020-11-21 21:53:31 +0100

    DB fetching added

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
 const express = require("express");
 const app = express();
-
+const mongoose = require("mongoose");
+require("dotenv").config({ path: __dirname + "/.env" });
 const router = express.Router();
+const offers = require("./model");
 
-router.get("/offers", (req, res) => {
-  return res.send("Received a GET HTTP method");
+const { DB_USER, DB_PASS, DB_PORT, DB_SERVER } = process.env;
+
+const uri = `mongodb://${DB_USER}:${encodeURIComponent(
+  DB_PASS
+)}@${DB_SERVER}:${DB_PORT}`;
+
+mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true });
+
+router.get("/offers", async (req, res) => {
+  console.log("get offers");
+  try {
+    const dbOffers = await offers.find({});
+    return res.send(dbOffers);
+  } catch (ex) {
+    return res.status(400).send({
+      message: "An error occured",
+    });
+  }
 });
 
 app.use("/api", router);
 
-app.listen(80, () => console.log(`Example app listening on port 80`));
+app.listen(80, () => console.log(`App listening on port 80`));
